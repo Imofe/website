232a32d70ec3dcde156320ab1c9afee73381dbd4
Roman <romanlabzin03@gmail.com>
2023-03-24 12:35:52 +0500

    пропсы, две страницы круто!!!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
 import './App.css';
-import Profile from './components/Profile';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import {Route, Routes, BrowserRouter} from "react-router-dom";
+import Dialogs from './components/Dialogs/Dialogs';
+import Profile from "./components/Profile/Profile";
 
-function App() {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+const App = (props) => {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/' element={<Profile posts={props.state.profilePage}/>}/>
+                        <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage}/>}/>
+                        <Route path='/profile' element={<Profile state={props.state.profilePage}/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
-
 export default App;
